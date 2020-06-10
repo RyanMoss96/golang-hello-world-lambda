0f7d2a8df9c046438e1ba97c9263c1e63af7cb35
Ryan Moss <rmoss@localhost.localdomain>
2020-06-10 16:45:56 -0400

    SAM Template

--- main.go
+++ main.go
@@ -11,8 +11,12 @@ type MyEvent struct {
 	Name string `json:"name"`
 }
 
+type MyResponse struct {
+	Message string `json:"message:"`
+}
+
 func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Phil Sucks at Valorant"), nil
+	return MyResponse{Message: fmt.Sprintf("Hello World")}, nil
 }
 
 func main() {
